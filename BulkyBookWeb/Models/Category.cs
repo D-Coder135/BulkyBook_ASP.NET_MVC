93577312cb85257e6c585d2db3949a15c7d2fd34
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-12-28 22:42:38 +0530

    add the [Key] annotation for the Id() property method

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,7 +1,10 @@
-﻿namespace BulkyBookWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
 
         public string Name { get; set; }
