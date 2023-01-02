c251cf6549daed1f011a7624a4131fdec8b2fbc2
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-01-02 23:20:00 +0530

    add a parametrized constructor and initialize the _db field inside it's body

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -6,6 +6,12 @@ namespace BulkyBookWeb.Controllers
     public class CategoryController : Controller
     {
         private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
