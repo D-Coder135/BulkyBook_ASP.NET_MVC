2a30e5107f864a93caebc426b8f5f8059a4594f7
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-01-04 22:43:44 +0530

    add custom ErrorMessage inside the [Range()] attribute

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -12,7 +12,7 @@ namespace BulkyBookWeb.Models
         public string Name { get; set; }
 
         [DisplayName("Display Order")]
-        [Range(1, 100)]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1 and 100 only!!")]
         public int DisplayOrder { get; set; }
 
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
