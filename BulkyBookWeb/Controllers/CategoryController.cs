aa4e61aecf9fb40d18062303b37eb6bceeccacf2
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-01-06 22:21:22 +0530

    add some duplicate code along with the DeletePost(int? id) method

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -78,5 +78,41 @@ namespace BulkyBookWeb.Controllers
             }
             return View(categoryObj);
         }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            // var categoryFromDb = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            // var categoryFromDb = _db.Categories.SingleOrDefault(u=>u.Id==id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+        }
     }
 }
