4be244e60f60d804a3c999008f8bc3ed40b8ad83
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-01-23 22:16:42 +0530

    update the namespace for the ApplicationDbContext.cs file

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -1,11 +1,11 @@
 ﻿using BulkyBookWeb.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace BulkyBookWeb.Data
+namespace BulkyBook.DataAccess
 {
-    public class ApplicationDbContext: DbContext
+    public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
 
