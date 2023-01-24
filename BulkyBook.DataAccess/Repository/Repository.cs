467060f95cb34acbd2d3cb5b1e2a5d3caaf67ae5
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-01-24 23:26:15 +0530

    add a parametrized constructor inside which intialize the private field _db

--- BulkyBook.DataAccess/Repository/Repository.cs
+++ BulkyBook.DataAccess/Repository/Repository.cs
@@ -12,6 +12,11 @@ namespace BulkyBook.DataAccess.Repository
     {
         private readonly ApplicationDbContext _db;
 
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public void Add(T entity)
         {
             throw new NotImplementedException();
