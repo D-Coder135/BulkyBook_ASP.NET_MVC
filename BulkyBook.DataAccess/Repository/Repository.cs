750381b05e78adae3d6326081f948663ff38f6bb
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-01-24 23:22:45 +0530

    implement the IRepository interface inside the Repository class

--- BulkyBook.DataAccess/Repository/Repository.cs
+++ BulkyBook.DataAccess/Repository/Repository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BulkyBook.DataAccess.Repository
 {
-    public class Repository
+    public class Repository<T> : IRepository<T> where T : class
     {
     }
 }
