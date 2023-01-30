ac0062965072e9b26bfcd6d37cb9ae0f816732d2
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-01-30 22:36:22 +0530

    implement the IRepository<Category> inside the interface ICategoryRepository

--- BulkyBook.DataAccess/Repository/IRepository/ICategoryRepository.cs
+++ BulkyBook.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BulkyBook.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace BulkyBook.DataAccess.Repository.IRepository
 {
-    public interface ICategoryRepository
+    public interface ICategoryRepository : IRepository<Category>
     {
     }
 }
