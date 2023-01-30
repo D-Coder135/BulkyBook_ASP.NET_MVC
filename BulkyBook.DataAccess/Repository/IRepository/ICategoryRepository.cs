8a2e195e057e682ab8d4f1d8bee86ce542b578fb
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-01-30 23:15:40 +0530

    remove some part of the code

--- BulkyBook.DataAccess/Repository/IRepository/ICategoryRepository.cs
+++ BulkyBook.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -10,7 +10,5 @@ namespace BulkyBook.DataAccess.Repository.IRepository
     public interface ICategoryRepository : IRepository<Category>
     {
         void Update(Category category);
-
-        void Save();
     }
 }
