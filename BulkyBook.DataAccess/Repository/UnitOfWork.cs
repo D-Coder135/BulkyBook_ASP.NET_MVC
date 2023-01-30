a01ffedd3c5bd4ab02534654561a7a4c7a24be08
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-01-30 23:10:37 +0530

    implement the interface IUnitOfWork inside the UnitOf Work class

--- BulkyBook.DataAccess/Repository/UnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace BulkyBook.DataAccess.Repository
 {
-    public class UnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
     }
 }
