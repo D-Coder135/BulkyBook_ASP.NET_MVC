59fe892a87bfb43daddd4be119a4d4bf93f317b7
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-01-30 23:17:21 +0530

    inject the IUnitOfWork and UnitOfWork interface and the class using the dependency injection

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlSer
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
 
-builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 
