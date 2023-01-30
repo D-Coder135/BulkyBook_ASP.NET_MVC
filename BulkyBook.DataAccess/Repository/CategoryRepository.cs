ad0c1e5dabfb77e5bb68e202a181d30b8fbfdd92
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-01-30 22:50:43 +0530

    add the base(db) to pass the field _db field as the parameter to the base class Repository

--- BulkyBook.DataAccess/Repository/CategoryRepository.cs
+++ BulkyBook.DataAccess/Repository/CategoryRepository.cs
@@ -12,7 +12,7 @@ namespace BulkyBook.DataAccess.Repository
     {
         private ApplicationDbContext _db;
 
-        public CategoryRepository(ApplicationDbContext db)
+        public CategoryRepository(ApplicationDbContext db) : base(db)
         {
             _db = db;
         }
