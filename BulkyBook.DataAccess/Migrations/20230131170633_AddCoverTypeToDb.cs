66c36434423285ddb6075fc83ae44a692e340901
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-01-31 22:37:57 +0530

    add the migration for AddCoverTypeToDb and then run the command update-database in order to add the table to the database

---
+++ BulkyBook.DataAccess/Migrations/20230131170633_AddCoverTypeToDb.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyBookWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCoverTypeToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
