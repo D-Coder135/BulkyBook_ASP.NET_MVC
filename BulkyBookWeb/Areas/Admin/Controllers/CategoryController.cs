592035bf21e2c85d12e58166f2564cad8e9b98be
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-01 23:09:59 +0530

    add the Area inside the CategoryController.cs class

--- BulkyBookWeb/Areas/Admin/Controllers/CategoryController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
