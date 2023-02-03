b391702db0e79032f3d9631e64943a3caa5b7bd1
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-03 16:51:32 +0530

    initialize a field with FirstOrDefault() method call

--- BulkyBook.DataAccess/Repository/ProductRepository.cs
+++ BulkyBook.DataAccess/Repository/ProductRepository.cs
@@ -19,6 +19,7 @@ namespace BulkyBook.DataAccess.Repository
 
         public void Update(Product obj)
         {
+            var objFromDb = _db.Products.FirstOrDefault(u => u.Id == obj.Id);
 
         }
     }
