3e4941c6ede6eb7cb6a6e2b411eeaf49c327b3db
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-03 15:55:05 +0530

    add different properties inside the Product class along with the getter and setter

--- BulkyBook.Models/Product.cs
+++ BulkyBook.Models/Product.cs
@@ -8,5 +8,26 @@ namespace BulkyBook.Models
 {
     public class Product
     {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string ISBN { get; set; }
+
+        public string Author { get; set; }
+
+        public double ListPrice { get; set; }
+
+        public double Price { get; set; }
+
+        public double Price50 { get; set; }
+
+        public double Price100 { get; set; }
+
+        public string ImageUrl { get; set; }
+
+
     }
 }
