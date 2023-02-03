b88b6032fe797dfc08e58b2b80e31951fa14d66f
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-03 16:58:05 +0530

    add some fix up to resolve the errors

--- BulkyBook.DataAccess/Repository/IRepository/IProductRepository.cs
+++ BulkyBook.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 
 namespace BulkyBook.DataAccess.Repository.IRepository
 {
-    public interface ICoverTypesRepository : IRepository<CoverType>
+    public interface IProductRepository : IRepository<Product>
     {
-        void Update(CoverType category);
+        void Update(Product obj);
     }
 }
