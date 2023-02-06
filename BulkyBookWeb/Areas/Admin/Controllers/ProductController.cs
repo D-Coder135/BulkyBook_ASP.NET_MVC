fb5c1ee8afaf063d5039296401f1ab3f663825f3
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-06 13:50:29 +0530

    initialize an object of type ViewBag

--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -43,7 +43,7 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
             if (id == null || id == 0)
             {
                 // create product
-
+                ViewBag.CategoryList = CategoryList;
                 return View(product);
             }
             else
