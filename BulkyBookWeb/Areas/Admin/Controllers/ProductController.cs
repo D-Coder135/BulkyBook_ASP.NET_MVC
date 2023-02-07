d92fa34d6478c1d9b1bfeccd8162315a30c86b30
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-07 14:44:00 +0530

    add some fix up to the POST Upsert() method

--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -59,11 +59,11 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
         //POST
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Edit(CoverType obj)
+        public IActionResult Upsert(ProductVM obj, IFormFile file)
         {
             if (ModelState.IsValid)
             {
-                _unitOfWork.CoverTypes.Update(obj);
+                /*_unitOfWork.CoverTypes.Update(obj);*/
                 _unitOfWork.Save();
                 TempData["success"] = "CoverType Updated Successfully";
                 return RedirectToAction("Index");
