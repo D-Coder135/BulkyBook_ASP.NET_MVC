98c3ba43a11ba514d2b0d8379ae27b858dcf1f8a
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-07 14:25:48 +0530

    paste the cut out part of the code inside the ProductVM.cs and also install the Microsoft.AspNetCore.Mvc.ViewFeatures NuGet package

--- BulkyBook.Models/ViewModels/ProductVM.cs
+++ BulkyBook.Models/ViewModels/ProductVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,20 @@ namespace BulkyBook.Models.ViewModels
 {
     public class ProductVM
     {
+        Product product = new();
+        IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(
+            u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            }
+            );
+        IEnumerable<SelectListItem> CoverTypeList = _unitOfWork.CoverTypes.GetAll().Select(
+            u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            }
+            );
     }
 }
