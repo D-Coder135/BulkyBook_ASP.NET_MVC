716296499d5e91955782c8f75ca4a23a81ea6445
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-07 22:53:30 +0530

    add a string nullable parameter inside the IRepository.cs file

--- BulkyBook.DataAccess/Repository/IRepository/IRepository.cs
+++ BulkyBook.DataAccess/Repository/IRepository/IRepository.cs
@@ -9,9 +9,9 @@ namespace BulkyBook.DataAccess.Repository.IRepository
 {
     public interface IRepository<T> where T : class
     {
-        T GetFirstOrDefault(Expression<Func<T, bool>> filter);
+        T GetFirstOrDefault(Expression<Func<T, bool>> filter, string? includeProperties = null);
 
-        IEnumerable<T> GetAll();
+        IEnumerable<T> GetAll(string? includeProperties = null);
 
         void Add(T entity);
 
