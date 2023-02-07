56420d04d2926168fa98375ef9cf231bdee06ac3
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-07 22:35:58 +0530

    add the ajax and columns inside the DataTable() method call

--- BulkyBookWeb/wwwroot/js/product.js
+++ BulkyBookWeb/wwwroot/js/product.js
@@ -5,5 +5,15 @@ $(document).ready(function () {
 });
 
 function loadDataTable() {
-
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/Product/GetAll"
+        },
+        "columns": [
+            { "data": "title", "width": "15%" },
+            { "data": "isbn", "width": "15%" },
+            { "data": "price", "width": "15%" },
+            { "data": "author", "width": "15%" },
+        ]
+    });
 }
