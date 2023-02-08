cc03a1470790037a1adac50a56e86ce168ef9910
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-08 14:12:50 +0530

    add the [Display(Name)] annotation

--- BulkyBook.Models/Product.cs
+++ BulkyBook.Models/Product.cs
@@ -29,26 +29,31 @@ namespace BulkyBook.Models
 
         [Required]
         [Range(1, 10000)]
+        [Display(Name = "Price for 1-50")]
         public double Price { get; set; }
 
         [Required]
         [Range(1, 10000)]
+        [Display(Name = "Price for 50-100")]
         public double Price50 { get; set; }
 
         [Required]
         [Range(1, 10000)]
+        [Display(Name = "Price for 100+")]
         public double Price100 { get; set; }
 
         [ValidateNever]
         public string ImageUrl { get; set; }
 
         [Required]
+        [Display(Name = "Category")]
         public int CategoryId { get; set; }
 
         [ValidateNever]
         public Category Category { get; set; }
 
         [Required]
+        [Display(Name = "CoverType")]
         public int CoverTypeId { get; set; }
 
         [ValidateNever]
