e683caad7b7e53eeab2c93ec4f891e693cf166bf
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-08 14:03:18 +0530

    paste some code inside the return statement of the function inside the render property a column

--- BulkyBookWeb/wwwroot/js/product.js
+++ BulkyBookWeb/wwwroot/js/product.js
@@ -15,6 +15,18 @@ function loadDataTable() {
             { "data": "price", "width": "15%" },
             { "data": "author", "width": "15%" },
             { "data": "category.name", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <div class="w-75 btn-group" role="group">
+                        <a asp-controller="Category" asp-action="Edit" asp-route-id="@obj.Id" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i>Edit</a>
+                        <a asp-controller="Category" asp-action="Delete" asp-route-id="@obj.Id" class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i>Delete</a>
+                    </div>
+                        `
+                },
+                "width": "15%"
+            },
         ]
     });
 }
