40377f3c45c8570d7e8e6a24ec7a525b8bcaf8b0
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-08 14:05:17 +0530

    add the href property for the anchor tag

--- BulkyBookWeb/wwwroot/js/product.js
+++ BulkyBookWeb/wwwroot/js/product.js
@@ -20,8 +20,8 @@ function loadDataTable() {
                 "render": function (data) {
                     return `
                         <div class="w-75 btn-group" role="group">
-                        <a asp-controller="Category" asp-action="Edit" asp-route-id="@obj.Id" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i>Edit</a>
-                        <a asp-controller="Category" asp-action="Delete" asp-route-id="@obj.Id" class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i>Delete</a>
+                        <a href="/Admin/Product/Upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i>Edit</a>
+                        <a  class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i>Delete</a>
                     </div>
                         `
                 },
