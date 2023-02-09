7ac559b353402309e53c0fdd14d8be14f12a65fd
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-09 23:14:40 +0530

    add some fix up to the code

--- BulkyBookWeb/wwwroot/js/product.js
+++ BulkyBookWeb/wwwroot/js/product.js
@@ -21,7 +21,7 @@ function loadDataTable() {
                     return `
                         <div class="w-75 btn-group" role="group">
                         <a href="/Admin/Product/Upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i>Edit</a>
-                        <a onClick=Delete('/Admin/Product/'+${data}) class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i>Delete</a>
+                        <a onClick=Delete('/Admin/Product/Delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i>Delete</a>
                     </div>
                         `
                 },
