97b8b8b905ef53e29b20b30644821497ac458ac8
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-13 23:01:30 +0530

    update the code inside the CompanyController.cs class

--- BulkyBookWeb/Areas/Admin/Controllers/CompanyController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/CompanyController.cs
@@ -8,15 +8,13 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 namespace BulkyBookWeb.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    public class ProductController : Controller
+    public class CompanyController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        private readonly IWebHostEnvironment _hostEnvironment;
 
-        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
+        public CompanyController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
-            _hostEnvironment = hostEnvironment;
         }
 
         public IActionResult Index()
@@ -27,75 +25,38 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
         //GET
         public IActionResult Upsert(int? id)
         {
-            ProductVM productVM = new()
-            {
-                Product = new(),
-                CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem
-                {
-                    Text = i.Name,
-                    Value = i.Id.ToString()
-                }),
-                CoverTypeList = _unitOfWork.CoverTypes.GetAll().Select(i => new SelectListItem
-                {
-                    Text = i.Name,
-                    Value = i.Id.ToString()
-                })
-            };
+            Company company = new();
 
             if (id == null || id == 0)
             {
-                // create product
-                /*ViewBag.CategoryList = CategoryList;
-                ViewData["CoverTypeList"] = CoverTypeList;*/
-                return View(productVM);
+
+                return View(company);
             }
             else
             {
-                productVM.Product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id);
-                return View(productVM);
-                // update product
+                company = _unitOfWork.Company.GetFirstOrDefault(u => u.Id == id);
+                return View(company);
             }
         }
 
         //POST
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Upsert(ProductVM obj, IFormFile? file)
+        public IActionResult Upsert(Company obj, IFormFile? file)
         {
             if (ModelState.IsValid)
             {
-                string wwwRootPath = _hostEnvironment.WebRootPath;
-                if (file != null)
+                if (obj.Id == 0)
                 {
-                    string fileName = Guid.NewGuid().ToString();
-                    var uploads = Path.Combine(wwwRootPath, @"images\products");
-                    var extension = Path.GetExtension(file.FileName);
-
-                    if (obj.Product.ImageUrl != null)
-                    {
-                        var oldImagePath = Path.Combine(wwwRootPath, obj.Product.ImageUrl.TrimStart('\\'));
-                        if (System.IO.File.Exists(oldImagePath))
-                        {
-                            System.IO.File.Delete(oldImagePath);
-                        }
-                    }
-
-                    using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
-                    {
-                        file.CopyTo(fileStreams);
-                    }
-                    obj.Product.ImageUrl = @"\images\products\" + fileName + extension;
-                }
-                if (obj.Product.Id == 0)
-                {
-                    _unitOfWork.Product.Add(obj.Product);
+                    _unitOfWork.Company.Add(obj);
+                    TempData["success"] = "Company Created Successfully";
                 }
                 else
                 {
-                    _unitOfWork.Product.Update(obj.Product);
+                    _unitOfWork.Company.Update(obj);
+                    TempData["success"] = "Company Updated Successfully";
                 }
                 _unitOfWork.Save();
-                TempData["success"] = "Product Created Successfully";
                 return RedirectToAction("Index");
             }
             return View(obj);
@@ -105,27 +66,21 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            var productList = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
-            return Json(new { data = productList });
+            var companyList = _unitOfWork.Company.GetAll();
+            return Json(new { data = companyList });
         }
 
         //POST
         [HttpDelete]
         public IActionResult Delete(int? id)
         {
-            var obj = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id);
+            var obj = _unitOfWork.Company.GetFirstOrDefault(u => u.Id == id);
             if (obj == null)
             {
                 return Json(new { success = false, message = "Error while deleting" });
             }
 
-            var oldImagePath = Path.Combine(_hostEnvironment.WebRootPath, obj.ImageUrl.TrimStart('\\'));
-            if (System.IO.File.Exists(oldImagePath))
-            {
-                System.IO.File.Delete(oldImagePath);
-            }
-
-            _unitOfWork.Product.Remove(obj);
+            _unitOfWork.Company.Remove(obj);
             _unitOfWork.Save();
             return Json(new { success = true, message = "Deleted Successfully" });
 
