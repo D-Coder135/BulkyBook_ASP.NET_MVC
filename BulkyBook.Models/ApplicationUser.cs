d8f3e4965a3429ba77e953ccd621b521f60e935a
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-13 22:22:36 +0530

    add a public string property Name with the Required annotation

--- BulkyBook.Models/ApplicationUser.cs
+++ BulkyBook.Models/ApplicationUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,7 @@ namespace BulkyBook.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Required]
+        public string Name { get; set; }
     }
 }
