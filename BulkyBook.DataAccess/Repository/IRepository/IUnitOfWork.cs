d7f66ad5e967f2cc516bbed9085051984643ed2d
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-13 22:53:21 +0530

    add the ICompanyRepository inside the IUnitOfWork interfact

--- BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -14,6 +14,8 @@ namespace BulkyBook.DataAccess.Repository.IRepository
 
         IProductRepository Product { get; }
 
+        ICompanyRepository Company { get; }
+
         void Save();
     }
 }
