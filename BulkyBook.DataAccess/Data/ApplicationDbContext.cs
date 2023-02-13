925b6a10d103d92c757980ad5dade51340be6b04
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-13 22:25:04 +0530

    add a DbSet for the ApplicationUser inside the ApplicatioDbContext file

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -13,5 +13,7 @@ namespace BulkyBook.DataAccess
         public DbSet<Category> Categories { get; set; }
         public DbSet<CoverType> CoverTypes { get; set; }
         public DbSet<Product> Products { get; set; }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
