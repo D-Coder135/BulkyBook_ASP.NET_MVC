014355da33d7f3a99054abe7bf48de00146c8568
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-13 22:23:38 +0530

    add some more public properties along with the getter and setter methods

--- BulkyBook.Models/ApplicationUser.cs
+++ BulkyBook.Models/ApplicationUser.cs
@@ -12,5 +12,13 @@ namespace BulkyBook.Models
     {
         [Required]
         public string Name { get; set; }
+
+        public string? StreetAddress { get; set; }
+
+        public string? City { get; set; }
+
+        public string? State { get; set; }
+
+        public string? PostalCode { get; set; }
     }
 }
