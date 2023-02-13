06bb8d82cffa5c2ddb3bc128004d578713c81f0e
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-13 14:41:32 +0530

    add the MapRazorPages() method call inside the Program.cs file

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -35,7 +35,7 @@ app.UseRouting();
 app.UseAuthentication(); ;
 
 app.UseAuthorization();
-
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
