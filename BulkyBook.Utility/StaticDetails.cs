d06ec3bfe203504700295b3a4049abbb7022ca5b
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-14 15:04:26 +0530

    add some public const fields inside the StaticDetails class

--- BulkyBook.Utility/StaticDetails.cs
+++ BulkyBook.Utility/StaticDetails.cs
@@ -8,6 +8,9 @@ namespace BulkyBook.Utility
 {
     public static class StaticDetails
     {
-
+        public const string Role_User_Indi = "Individual";
+        public const string Role_User_Comp = "Company";
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
     }
 }
