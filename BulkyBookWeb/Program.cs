42285c1cb7af8a94a324a7704c8201fb2a55d1f8
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-14 15:22:40 +0530

    add some fix up to the code

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -11,6 +11,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddRazorPages();
+
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
