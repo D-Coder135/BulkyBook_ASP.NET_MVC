12e9274cd0187659873fd585c643bbba832b595e
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-14 14:58:15 +0530

    modify the DefaultIdentity() method call with the AddIdentity() method call in order to add custom roles for the application

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlSer
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
 
-builder.Services.AddDefaultIdentity<IdentityUser>()
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultTokenProviders()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
