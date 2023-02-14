2c5a85b8aed8a0a5b506e464cbbbc0835b901d88
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-14 15:13:10 +0530

    add a return statement

--- BulkyBook.Utility/EmailSender.cs
+++ BulkyBook.Utility/EmailSender.cs
@@ -9,5 +9,9 @@ namespace BulkyBook.Utility
 {
     public class EmailSender : IEmailSender
     {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            return Task.CompletedTask;
+        }
     }
 }
