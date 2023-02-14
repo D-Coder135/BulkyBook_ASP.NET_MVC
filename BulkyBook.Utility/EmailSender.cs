7b27395c4d0db3bdce6d13887e611be77da96f56
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-14 15:12:50 +0530

    implement the interface IEmailSender along with the required method implementation inside the class EmailSender

--- BulkyBook.Utility/EmailSender.cs
+++ BulkyBook.Utility/EmailSender.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace BulkyBook.Utility
 {
-    public class EmailSender
+    public class EmailSender : IEmailSender
     {
     }
 }
