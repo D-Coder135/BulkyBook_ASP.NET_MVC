02c00d1ae0f8118fd3c28dd1ab5da98ce95bc808
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-15 12:43:27 +0530

    add the ForeignKey and ValidateNever annotation

--- BulkyBook.Models/ShoppingCart.cs
+++ BulkyBook.Models/ShoppingCart.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,8 @@ namespace BulkyBook.Models
         public int Id { get; set; }
 
         public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        [ValidateNever]
 
         public Product Product { get; set; }
 
