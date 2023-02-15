ce56f00750d86013fe64809242baffffa2cd8a70
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-15 13:58:01 +0530

    modify some code inside the IApplicationUserRepository

--- BulkyBook.DataAccess/Repository/IRepository/IApplicationUserRepository.cs
+++ BulkyBook.DataAccess/Repository/IRepository/IApplicationUserRepository.cs
@@ -7,8 +7,7 @@ using System.Threading.Tasks;
 
 namespace BulkyBook.DataAccess.Repository.IRepository
 {
-    public interface ICategoryRepository : IRepository<Category>
+    public interface IApplicationUserRepository : IRepository<ApplicationUser>
     {
-        void Update(Category category);
     }
 }
