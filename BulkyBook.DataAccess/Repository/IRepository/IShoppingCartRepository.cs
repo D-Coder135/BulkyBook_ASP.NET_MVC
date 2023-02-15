0cec6cb24db07eddee084f1c912f7d76eec8fbdd
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-15 22:40:20 +0530

    add a method of type int DecrementCount() along with parameters of type ShoppingCart and int respectively

--- BulkyBook.DataAccess/Repository/IRepository/IShoppingCartRepository.cs
+++ BulkyBook.DataAccess/Repository/IRepository/IShoppingCartRepository.cs
@@ -10,5 +10,7 @@ namespace BulkyBook.DataAccess.Repository.IRepository
     public interface IShoppingCartRepository : IRepository<ShoppingCart>
     {
         int IncrementCount(ShoppingCart shoppingCart, int count);
+
+        int DecrementCount(ShoppingCart shoppingCart, int count);
     }
 }
