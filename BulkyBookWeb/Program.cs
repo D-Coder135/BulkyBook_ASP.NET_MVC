c516dc1dc4b20313e40513e6e1ef591378658d2d
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-15 22:52:19 +0530

    call the ConfigureApplicationCookie() inside the Program.cs file to configure the default route for some unknown actions

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -22,6 +22,12 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultTokenProvid
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddSingleton<IEmailSender, EmailSender>();
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = $"/Identity/Account/Login";
+    options.LogoutPath = $"/Identity/Account/Logout";
+    options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+});
 
 var app = builder.Build();
 
