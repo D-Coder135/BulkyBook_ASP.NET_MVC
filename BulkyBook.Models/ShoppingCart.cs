4fa0716ed1bcfbac2a0fb11bfb754d4ff2cce7ce
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-15 12:44:28 +0530

    add a public string property ApplicationUserId

--- BulkyBook.Models/ShoppingCart.cs
+++ BulkyBook.Models/ShoppingCart.cs
@@ -15,12 +15,14 @@ namespace BulkyBook.Models
         public int Id { get; set; }
 
         public int ProductId { get; set; }
+
         [ForeignKey("ProductId")]
         [ValidateNever]
-
         public Product Product { get; set; }
 
         [Range(1, 1000, ErrorMessage = "Please enter a value in between 1 and 1000")]
         public int Count { get; set; }
+
+        public string ApplicationUserId { get; set; }
     }
 }
