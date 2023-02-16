5ab13b2f1ff29aa215103bb38880c3d8cacccb86
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-16 22:41:16 +0530

    made the associated change inside the GetAll() method definition inside the IRepository interface

--- BulkyBook.DataAccess/Repository/IRepository/IRepository.cs
+++ BulkyBook.DataAccess/Repository/IRepository/IRepository.cs
@@ -11,7 +11,7 @@ namespace BulkyBook.DataAccess.Repository.IRepository
     {
         T GetFirstOrDefault(Expression<Func<T, bool>> filter, string? includeProperties = null);
 
-        IEnumerable<T> GetAll(string? includeProperties = null);
+        IEnumerable<T> GetAll(Expression<Func<T, bool>> filter, string? includeProperties = null);
 
         void Add(T entity);
 
