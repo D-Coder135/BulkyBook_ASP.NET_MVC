d4a4d6b10084fbb911f4d320f74c686bf5ca1c89
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-16 22:29:54 +0530

    add the Authorize annotation for the CartController class

--- BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Areas.Customer.Controllers
 {
     [Area("Customer")]
+    [Authorize]
     public class CartController : Controller
     {
         public IActionResult Index()
