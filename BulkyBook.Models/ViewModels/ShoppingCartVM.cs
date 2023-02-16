ace465e3dbc7aa7336a44af7131e8c12d0ce2881
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-16 23:17:51 +0530

    add a public property of type double

--- BulkyBook.Models/ViewModels/ShoppingCartVM.cs
+++ BulkyBook.Models/ViewModels/ShoppingCartVM.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace BulkyBook.Models.ViewModels
 {
-    public class ShoppingCartVM
-    {
-        public IEnumerable<ShoppingCart> ListCart { get; set; }
-    }
+	public class ShoppingCartVM
+	{
+		public IEnumerable<ShoppingCart> ListCart { get; set; }
+
+		public double CartTotal { get; set; }
+	}
 }
