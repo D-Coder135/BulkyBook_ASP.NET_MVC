808bacb5397c9671cb8b1218aa8d6a910308f486
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-16 23:15:42 +0530

    update the value for the cart.Price property with the method call of GetPriceBasedOnQuantity() method

--- BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -30,7 +30,7 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
 
 			foreach (var cart in ShoppingCartVM.ListCart)
 			{
-
+				cart.Price = GetPriceBasedOnQuantity(cart.Count, cart.Product.Price, cart.Product.Price50, cart.Product.Price100);
 			}
 
 			return View(ShoppingCartVM);
