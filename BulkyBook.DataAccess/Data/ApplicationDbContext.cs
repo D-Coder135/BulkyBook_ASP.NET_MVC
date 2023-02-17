ddbf624c6b5c1089eb667d72eb7fb72c7cfe087c
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-17 22:30:26 +0530

    add public DbSet for OrderDetail and OrderHeader repectively inside the ApplicationDbContext

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -4,18 +4,20 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BulkyBook.DataAccess
 {
-    public class ApplicationDbContext : IdentityDbContext
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
-        }
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+		}
 
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<CoverType> CoverTypes { get; set; }
-        public DbSet<Product> Products { get; set; }
-        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
-        public DbSet<Company> Companies { get; set; }
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<CoverType> CoverTypes { get; set; }
+		public DbSet<Product> Products { get; set; }
+		public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+		public DbSet<Company> Companies { get; set; }
 
-        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
-    }
+		public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+		public DbSet<OrderHeader> OrderHeaders { get; set; }
+		public DbSet<OrderDetail> OrderDetails { get; set; }
+	}
 }
