1e4d7497db8bac358725b4b7c52982728c249bba
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-17 22:28:16 +0530

    add some required properties inside the OrderDetail class

--- BulkyBook.Models/OrderDetail.cs
+++ BulkyBook.Models/OrderDetail.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,17 @@ namespace BulkyBook.Models
 {
 	public class OrderDetail
 	{
+		public int Id { get; set; }
+		[Required]
+		public int OrderId { get; set; }
+		[ForeignKey("OrderId")]
+		public OrderHeader OrderHeader { get; set; }
+
+		[Required]
+		public int ProductId { get; set; }
+		[ForeignKey("ProductId")]
+		public Product Product { get; set; }
+		public int Count { get; set; }
+		public double Price { get; set; }
 	}
 }
