fba67e9f4247ada085b444fc2d89f06ed9c5af83
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-17 22:23:28 +0530

    add some required properties along with different annotations

--- BulkyBook.Models/OrderHeader.cs
+++ BulkyBook.Models/OrderHeader.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,39 @@ namespace BulkyBook.Models
 {
 	public class OrderHeader
 	{
+		public int Id { get; set; }
+		public string ApplicationUserId { get; set; }
+		[ForeignKey("ApplicationUserId")]
+		[ValidateNever]
+		public ApplicationUser ApplicationUser { get; set; }
+
+		[Required]
+		public DateTime OrderDate { get; set; }
+		public DateTime ShippingDate { get; set; }
+		public double OrderTotal { get; set; }
+		public string? OrderStatus { get; set; }
+		public string? PaymentStatus { get; set; }
+		public string? TrackingNumber { get; set; }
+		public string? Carrier { get; set; }
+		public DateTime PaymentDate { get; set; }
+		public DateTime PaymentDueDate { get; set; }
+
+		public string? SessionId { get; set; }
+		public string? PaymentIntendId { get; set; }
+
+		[Required]
+		public string PhoneNumber { get; set; }
+		[Required]
+		public string StreetAddress { get; set; }
+		[Required]
+		public string City { get; set; }
+		[Required]
+		public string State { get; set; }
+		[Required]
+		public string PostalCode { get; set; }
+		[Required]
+		public string Name { get; set; }
+
+
 	}
 }
