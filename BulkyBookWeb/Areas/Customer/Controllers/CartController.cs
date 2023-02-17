56bd48145495fdef5b25f8f451c44608b04254dd
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-17 16:19:59 +0530

    call the ShoppingCart.IncrementCount() method

--- BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -41,6 +41,7 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
 		public IActionResult Plus(int cartId)
 		{
 			var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(u => u.Id == cartId);
+			_unitOfWork.ShoppingCart.IncrementCount(cart, 1);
 		}
 
 
