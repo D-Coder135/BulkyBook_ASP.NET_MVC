dedde061c54df356e86f7f31c7b2a29dfb9e84b1
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-22 23:22:56 +0530

    add the OrderDetail and OrderHeader inside the UnitOfWork class

--- BulkyBook.DataAccess/Repository/UnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -7,34 +7,38 @@ using System.Threading.Tasks;
 
 namespace BulkyBook.DataAccess.Repository
 {
-    public class UnitOfWork : IUnitOfWork
-    {
-        private ApplicationDbContext _db;
+	public class UnitOfWork : IUnitOfWork
+	{
+		private ApplicationDbContext _db;
 
-        public UnitOfWork(ApplicationDbContext db)
-        {
-            _db = db;
-            Category = new CategoryRepository(_db);
-            CoverTypes = new CoverTypesRepository(_db);
-            Product = new ProductRespository(_db);
-            Company = new CompanyRepository(_db);
-            ApplicationUser = new ApplicationUserRepository(_db);
-            ShoppingCart = new ShoppingCartRepository(_db);
-        }
-        public ICategoryRepository Category { get; private set; }
+		public UnitOfWork(ApplicationDbContext db)
+		{
+			_db = db;
+			Category = new CategoryRepository(_db);
+			CoverTypes = new CoverTypesRepository(_db);
+			Product = new ProductRespository(_db);
+			Company = new CompanyRepository(_db);
+			ApplicationUser = new ApplicationUserRepository(_db);
+			ShoppingCart = new ShoppingCartRepository(_db);
+			OrderHeader = new OrderHeaderRepository(_db);
+			OrderDetail = new OrderDetailRepository(_db);
+		}
+		public ICategoryRepository Category { get; private set; }
 
-        public ICoverTypesRepository CoverTypes { get; private set; }
+		public ICoverTypesRepository CoverTypes { get; private set; }
 
-        public IProductRepository Product { get; private set; }
-        public ICompanyRepository Company { get; private set; }
+		public IProductRepository Product { get; private set; }
+		public ICompanyRepository Company { get; private set; }
 
-        public IShoppingCartRepository ShoppingCart { get; private set; }
+		public IShoppingCartRepository ShoppingCart { get; private set; }
 
-        public IApplicationUserRepository ApplicationUser { get; private set; }
+		public IApplicationUserRepository ApplicationUser { get; private set; }
+		public IOrderHeaderRepository OrderHeader { get; private set; }
+		public IOrderDetailRepository OrderDetail { get; private set; }
 
-        public void Save()
-        {
-            _db.SaveChanges();
-        }
-    }
+		public void Save()
+		{
+			_db.SaveChanges();
+		}
+	}
 }
