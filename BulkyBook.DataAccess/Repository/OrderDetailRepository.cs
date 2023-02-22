691acee9735b6165b09825cf34d3ca1ecd819326
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-22 23:16:43 +0530

    modify some code inside the OrderDetailRepository file

--- BulkyBook.DataAccess/Repository/OrderDetailRepository.cs
+++ BulkyBook.DataAccess/Repository/OrderDetailRepository.cs
@@ -8,18 +8,18 @@ using System.Threading.Tasks;
 
 namespace BulkyBook.DataAccess.Repository
 {
-    public class CategoryRepository : Repository<Category>, ICategoryRepository
-    {
-        private ApplicationDbContext _db;
+	public class OrderDetailRepository : Repository<OrderDetail>, IOrderDetailRepository
+	{
+		private ApplicationDbContext _db;
 
-        public CategoryRepository(ApplicationDbContext db) : base(db)
-        {
-            _db = db;
-        }
+		public OrderDetailRepository(ApplicationDbContext db) : base(db)
+		{
+			_db = db;
+		}
 
-        public void Update(Category obj)
-        {
-            _db.Categories.Update(obj);
-        }
-    }
+		public void Update(OrderDetail obj)
+		{
+			_db.OrderDetails.Update(obj);
+		}
+	}
 }
