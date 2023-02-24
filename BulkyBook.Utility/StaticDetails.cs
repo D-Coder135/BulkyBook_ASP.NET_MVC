9bd32607f51c31c86223ddd9bba6cb1ec97291e3
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-24 15:00:25 +0530

    add some public const field of type string for different status and payment conditions

--- BulkyBook.Utility/StaticDetails.cs
+++ BulkyBook.Utility/StaticDetails.cs
@@ -6,11 +6,26 @@ using System.Threading.Tasks;
 
 namespace BulkyBook.Utility
 {
-    public static class StaticDetails
-    {
-        public const string Role_User_Indi = "Individual";
-        public const string Role_User_Comp = "Company";
-        public const string Role_Admin = "Admin";
-        public const string Role_Employee = "Employee";
-    }
+	public static class StaticDetails
+	{
+		public const string Role_User_Indi = "Individual";
+		public const string Role_User_Comp = "Company";
+		public const string Role_Admin = "Admin";
+		public const string Role_Employee = "Employee";
+
+		public const string StatusPending = "Pending";
+		public const string StatusApproved = "Approved";
+		public const string StatusInProcess = "Processing";
+		public const string StatusShipped = "Shipped";
+		public const string StatusCancelled = "Cancelled";
+		public const string StatusRefund = "Refunded";
+
+		public const string PaymentStatusPending = "Pending";
+		public const string PaymentStatusApproved = "Approved";
+		public const string PaymentStatusDelayedPayment = "ApprovedForDelayedPayment";
+		public const string PaymentStatusRejected = "Rejected";
+
+
+
+	}
 }
