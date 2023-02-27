8647e5eb66c308e283fb013441db3056138266f3
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-27 23:16:54 +0530

    paste some code inside the CartController.cs class

--- BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -4,6 +4,7 @@ using BulkyBook.Models.ViewModels;
 using BulkyBook.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Stripe.Checkout;
 using System.Security.Claims;
 
 namespace BulkyBookWeb.Areas.Customer.Controllers
@@ -108,6 +109,36 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
 				_unitOfWork.OrderDetail.Add(orderDetail);
 				_unitOfWork.Save();
 			}
+
+			var options = new SessionCreateOptions
+			{
+				LineItems = new List<SessionLineItemOptions>
+		{
+		  new SessionLineItemOptions
+		  {
+			PriceData = new SessionLineItemPriceDataOptions
+			{
+			  UnitAmount = 2000,
+			  Currency = "usd",
+			  ProductData = new SessionLineItemPriceDataProductDataOptions
+			  {
+				Name = "T-shirt",
+			  },
+			},
+			Quantity = 1,
+		  },
+		},
+				Mode = "payment",
+				SuccessUrl = "http://localhost:4242/success",
+				CancelUrl = "http://localhost:4242/cancel",
+			};
+
+			var service = new SessionService();
+			Session session = service.Create(options);
+
+			Response.Headers.Add("Location", session.Url);
+			return new StatusCodeResult(303);
+
 			_unitOfWork.ShoppingCart.RemoveRange(ShoppingCartVM.ListCart);
 			_unitOfWork.Save();
 			return RedirectToAction("Index", "Home");
