1c37d0839ed0d577c8259e1ba8bd4489eef98d34
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-03-01 22:40:19 +0530

    explicitily define the area for the OrderController class

--- BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Areas.Admin.Controllers
 {
+	[Area("Admin")]
 	public class OrderController : Controller
 	{
 		private readonly IUnitOfWork _unitOfWork;
