8c9bf0c1d02cd43822bc9c03734701a0888246e8
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-03-01 22:30:03 +0530

    add a new Controller OrderController inside the Admin folder

---
+++ BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+	public class OrderController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
