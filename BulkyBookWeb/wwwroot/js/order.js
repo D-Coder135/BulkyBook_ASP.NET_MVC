21490f5cd071891a584bfc3352758b02ee66b5f8
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-03-01 22:52:41 +0530

    modify some part of the code for better alignment of the contents

--- BulkyBookWeb/wwwroot/js/order.js
+++ BulkyBookWeb/wwwroot/js/order.js
@@ -10,23 +10,23 @@ function loadDataTable() {
             "url": "/Admin/Order/GetAll"
         },
         "columns": [
-            { "data": "id", "width": "15%" },
-            { "data": "name", "width": "15%" },
+            { "data": "id", "width": "5%" },
+            { "data": "name", "width": "25%" },
             { "data": "phoneNumber", "width": "15%" },
             { "data": "applicationUser.email", "width": "15%" },
             { "data": "orderStatus", "width": "15%" },
-            { "data": "orderTotal", "width": "15%" },
+            { "data": "orderTotal", "width": "10%" },
             {
                 "data": "id",
                 "render": function (data) {
                     return `
                         <div class="w-75 btn-group" role="group">
-                        <a href="/Admin/Order/Details?orderId=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i>Details</a>
+                        <a href="/Admin/Order/Details?orderId=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i></a>
                   
                     </div>
                         `
                 },
-                "width": "15%"
+                "width": "5%"
             },
         ]
     });
