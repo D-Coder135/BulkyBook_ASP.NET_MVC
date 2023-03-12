de494b8cda785175ccbdda9a6945bca18572fb6a
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-03-12 22:51:46 +0530

    modify some part of the code inside the OrderHeaderRepository class

--- BulkyBook.DataAccess/Repository/OrderHeaderRepository.cs
+++ BulkyBook.DataAccess/Repository/OrderHeaderRepository.cs
@@ -39,6 +39,7 @@ namespace BulkyBook.DataAccess.Repository
 		public void UpdateStripePaymentId(int id, string sessionId, string paymentIntentId)
 		{
 			var orderFromDb = _db.OrderHeaders.FirstOrDefault(u => u.Id == id);
+			orderFromDb.PaymentDate = DateTime.Now;
 			orderFromDb.SessionId = sessionId;
 			orderFromDb.PaymentIntendId = paymentIntentId;
 		}
