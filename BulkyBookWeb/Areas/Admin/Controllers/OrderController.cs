54d5ba84411756bdc0e1265c336feb2b0da25457
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-03-14 13:17:09 +0530

    update the value for the TempData["success"]

--- BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
@@ -56,6 +56,7 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
             }
             _unitOfWork.OrderHeader.Update(orderHeaderFromDb);
             _unitOfWork.Save();
+            TempData["success"] = "Order Details Updated Successfully.";
             return View(OrderVM);
         }
 
