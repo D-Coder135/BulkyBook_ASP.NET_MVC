7ae9621bd4e9cbfb121e17b544c673198bfd287e
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-03-22 14:50:01 +0530

    initialize an object emaiToSend and update its related properties

--- BulkyBook.Utility/EmailSender.cs
+++ BulkyBook.Utility/EmailSender.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,11 @@ namespace BulkyBook.Utility
     {
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-
+            var emailToSend = new MimeMessage();
+            emailToSend.From.Add(MailboxAddress.Parse("hello@dotnetmastery.com"));
+            emailToSend.To.Add(MailboxAddress.Parse(email));
+            emailToSend.Subject = subject;
+            emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlMessage };
         }
     }
 }
