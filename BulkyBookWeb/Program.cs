fe2f3cc730860dc25f6be597a831dcc46a170a22
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-03-23 16:59:20 +0530

    add a new session inside the Program.cs

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -31,6 +31,12 @@ builder.Services.ConfigureApplicationCookie(options =>
     options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
 });
 builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(100);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
 var app = builder.Build();
 
