d47b48fe41a3d452a4f55083cef1b8570ed314a4
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-03-25 12:49:06 +0530

    paste some part of the code from register.cshtml.cs inside the DbInitializer class

--- BulkyBook.DataAccess/DbInitializer/DbInitializer.cs
+++ BulkyBook.DataAccess/DbInitializer/DbInitializer.cs
@@ -8,6 +8,36 @@ namespace BulkyBook.DataAccess.DbInitializer
 {
     public class DbInitializer : IDbInitializer
     {
+
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IUserStore<IdentityUser> _userStore;
+        private readonly IUserEmailStore<IdentityUser> _emailStore;
+        private readonly ILogger<RegisterModel> _logger;
+        private readonly IEmailSender _emailSender;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public RegisterModel(
+            UserManager<IdentityUser> userManager,
+            IUserStore<IdentityUser> userStore,
+            SignInManager<IdentityUser> signInManager,
+            ILogger<RegisterModel> logger,
+            IEmailSender emailSender,
+            RoleManager<IdentityRole> roleManager,
+            IUnitOfWork unitOfWork
+            )
+        {
+            _userManager = userManager;
+            _userStore = userStore;
+            _emailStore = GetEmailStore();
+            _signInManager = signInManager;
+            _logger = logger;
+            _emailSender = emailSender;
+            _roleManager = roleManager;
+            _unitOfWork = unitOfWork;
+        }
+
         public void Initialize()
         {
             //migrations if they are not applied.
