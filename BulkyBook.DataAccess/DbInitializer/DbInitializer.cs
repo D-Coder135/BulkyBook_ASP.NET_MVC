3bb384dc4651b744e0653246bc3f3000394918ff
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-03-25 12:46:09 +0530

    implement the method Initialize() inside the class DbInitializer

--- BulkyBook.DataAccess/DbInitializer/DbInitializer.cs
+++ BulkyBook.DataAccess/DbInitializer/DbInitializer.cs
@@ -8,5 +8,9 @@ namespace BulkyBook.DataAccess.DbInitializer
 {
     public class DbInitializer : IDbInitializer
     {
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
